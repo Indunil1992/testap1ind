88959af9166cfb69d8a8c27e24ca361cd82fe037
Indunil1992 <indunil@adroitlogic.com>
2020-04-01 09:07:13 +0530

    update

--- testAp1Ind.js
+++ testAp1Ind.js
@@ -5,13 +5,14 @@ exports.handler = async (event) => {
     try {
         let data = await cognito_idp.listUsers({
             UserPoolId: "us-east-1_k1hHHeTr4",
-            Limit: 10
+            AttributesToGet: ['sub', 'name', 'given_name', 'family_name', 'middle_name', 'nickname', 'preferred_username', 'profile', 'picture', 'website', 'email', 'email_verified', 'gender', 'birthdate', 'zoneinfo', 'locale', 'phone_number', 'phone_number_verified', 'address', 'updated_at'],
+            Limit: 100
         }).promise();
-console.log("dataa")
-console.log(data)
+        console.log("dataa")
+        console.log(data)
     } catch (err) {
         console.log("errrrrrrr")
-console.log(err)
+        console.log(err)
         // error handling goes here
     };
 
